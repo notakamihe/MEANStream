8f06eab4fba7ea66fef2309a817d0ca4c3139013
notakamihe <notakamihe@gmail.com>
2021-10-31 07:08:01 -0500

    Added gitignore to uploads in server and restricted S3 functionality to production

--- server/s3.js
+++ server/s3.js
@@ -15,15 +15,17 @@ const s3 = new S3({
 })
 
 function uploadFile(file) {
+  if (process.env.NODE_ENV.trim() === "production") {
     const fileStream = fs.createReadStream(file.path)
 
     const uploadParams = {
-        Bucket: bucketName,
-        Body: fileStream,
-        Key: file.filename
+      Bucket: bucketName,
+      Body: fileStream,
+      Key: file.filename
     }
 
     return s3.upload(uploadParams).promise()
+  }
 }
 
 exports.uploadFile = uploadFile
